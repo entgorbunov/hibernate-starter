ef60f0e58f638fd67c631231ffb69eba8de6169d
Alexander Gorbunov <entgorbunov@gmail.com>
2025-01-03 13:28:08 +0500

    logging

--- src/main/java/com/dmdevtraining/HibernateRunner.java
+++ src/main/java/com/dmdevtraining/HibernateRunner.java
@@ -2,11 +2,14 @@ package com.dmdevtraining;
 
 import com.dmdevtraining.entity.User;
 import com.dmdevtraining.util.HibernateUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 import java.sql.SQLException;
 
+@Slf4j
 public class HibernateRunner {
     public static void main(String[] args) throws SQLException {
         User user = User.builder()
@@ -15,23 +18,26 @@ public class HibernateRunner {
             .lastname("Ivanov")
             .build();
 
-        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
-            try (Session session1 = sessionFactory.openSession()) {
+        log.info("User entity is in transient state, object {}", user);
 
-                session1.beginTransaction();
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory()) {
+            Session session1 = sessionFactory.openSession();
+            try (session1) {
+
+                Transaction transaction = session1.beginTransaction();
+                log.trace("Transaction is created, {}", transaction);
 
                 session1.saveOrUpdate(user);
+                log.trace("User is in persistent state: {}, session {}", user, session1);
 
                 session1.getTransaction().commit();
             }
 
-            try (Session session2 = sessionFactory.openSession()) {
+            log.warn("User is in detached state: {}, session is closed {}", user, session1);
 
-                session2.beginTransaction();
-
-                session2.getTransaction().commit();
-            }
-
+        } catch (Exception e) {
+            log.error("Exception occurred", e);
+            throw e;
         }
     }
 }
